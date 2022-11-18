ee5d7daedc6e8531004d4aa3a2d9ab9b45cc6050
Keanu Rivers <keanurivers404@gmail.com>
2022-11-17 20:19:16 -0700

    finish exit app code

--- Develop/app.js
+++ Develop/app.js
@@ -141,3 +141,117 @@ function addManager() {
         userChoice();
     });
 }
+
+function exitApplication() {
+    // process.exit(0);
+    var html = `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">`
+                var managerHtml = "";
+                var internHtml = "";
+                var engineerHtml = "";
+                for (i = 0; i < employeeList.length; i++){
+                    if (employeeList[i].role === "Manager"){
+                        managerHtml += `<div class="card employee-card">
+                        <div class="card-header">
+                            <h2 class="card-title">${employeeList[i].name}</h2>
+                            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employeeList[i].role}</h3>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">ID: ${employeeList[i].id}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${employeeList[i].email}">${employeeList[i].email}</a></li>
+                                <li class="list-group-item">Office number: ${employeeList[i].OfficeNumber}</li>
+                            </ul>
+                        </div>
+                    </div>`
+                    }else if (employeeList[i].role === "Engineer"){
+                        engineerHtml += `<div class="card employee-card">
+                        <div class="card-header">
+                            <h2 class="card-title">${employeeList[i].name}</h2>
+                            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employeeList[i].role}</h3>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">ID: ${employeeList[i].id}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${employeeList[i].email}">${employeeList[i].email}</a></li>
+                                <li class="list-group-item">GitHub Username: ${employeeList[i].github}</li>
+                            </ul>
+                        </div>
+                    </div>`
+                    }else if (employeeList[i].role === "Intern"){
+                        internHtml += `<div class="card employee-card">
+                        <div class="card-header">
+                            <h2 class="card-title">${employeeList[i].name}</h2>
+                            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employeeList[i].role}</h3>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">ID: ${employeeList[i].id}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${employeeList[i].email}">${employeeList[i].email}</a></li>
+                                <li class="list-group-item">School: ${employeeList[i].School}</li>
+                            </ul>
+                        </div>
+                    </div>`
+                    }
+                }
+
+                html += managerHtml + internHtml + engineerHtml
+                html += `</div>
+            </div>
+        </div>
+    </body>
+    
+    </html>`
+    fs.writeFile("./templates/main.html", html, function (err, data){
+        if (err)
+        throw err
+        console.log("Html generator");
+        process.exit(0);
+    })
+}
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
